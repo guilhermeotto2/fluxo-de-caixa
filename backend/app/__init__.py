cca8bccc5eacbc9443b9edb5bd32cb5be36a72f3
isaac-arantes-t <ra-23106516-2@alunos.unicesumar.edu.br>
2024-08-24 21:10:53 -0300

    teste banco

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -5,11 +5,15 @@ def create_app():
     app = Flask(__name__)
 
     # Configurações do banco de dados SQL Server
-    app.config['SQL_SERVER_DATABASE_URI'] = 'Driver={SQL Server};Server=server_name;Database=database_name;Trusted_Connection=yes;'
+    dados_conexao = (
+    "Driver={SQL Server};"
+    "Server=DESKTOP-P1CORCC\SQLEXPRESS01;"
+    "Database=FluxoDeCaixa;"
+    )
 
     # Conexão com o banco de dados
     try:
-        connection = pyodbc.connect(app.config['SQL_SERVER_DATABASE_URI'])
+        connection = pyodbc.connect(dados_conexao)
         print("Conexão bem-sucedida!")
     except Exception as e:
         print("Erro ao conectar com o banco de dados:", e)
--- backend/app/models.py
+++ backend/app/models.py
@@ -1,1 +1,38 @@
+import pyodbc
 # Aqui você definirá os modelos de dados
+dados_conexao = (
+    "Driver={SQL Server};"
+    "Server=DESKTOP-P1CORCC\SQLEXPRESS01;"
+    "Database=FluxoDeCaixa;"
+    )
+
+# Conexão com o banco de dados
+try:
+    conexao = pyodbc.connect(dados_conexao)
+    print("Conexão bem-sucedida!")
+except Exception as e:
+    print("Erro ao conectar com o banco de dados:", e)
+
+
+cursor = conexao.cursor()
+
+# Solicitando o ID do cliente para buscar o nome
+cliente_id = input("Informe o ID do cliente: ")
+
+# Consulta para obter o nome do cliente a partir do ID
+comando_select = f"SELECT NomeCliente FROM DimCliente WHERE ClienteID = {cliente_id}"
+
+# Executando a consulta
+cursor.execute(comando_select)
+
+# Recuperando o resultado
+resultado = cursor.fetchone()
+
+# Verificando se o cliente foi encontrado
+if resultado:
+    print(f"O nome do cliente com ID {cliente_id} é: {resultado[0]}")
+else:
+    print(f"Cliente com ID {cliente_id} não encontrado.")
+
+# Fechando a conexão
+conexao.close()
